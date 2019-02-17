e8ec690dacbb48cc45f0b397245cfc5f795a9b0f
Richard Cheng <rhocheng@g.ucla.edu>
2019-02-17 09:37:40 -0800

    Started work on paddle script

---
+++ Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour {
+
+	public bool leftSide;
+
+	private RigidBody2D rb;
+    // Start is called before the first frame update
+    void Start() {
+    	rb = GameObject.getComponent<RigidBody2D>(); // Get Rigidbody component from sprite
+        if (leftSide)
+        	rb.position.x = 0; // If on the left side, set x-position to 0
+        else
+        	rb.position.x = 10; // If on the right side, set x-position to something
+        rb.position.y = 0; // Paddles always start at the midline
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (leftSide) {
+        	if (Input.getButtonDown("LeftSide")) {
+        		// Do stuff
+        	}
+        }
+        else {
+        	// Right side
+        }
+    }
+}
